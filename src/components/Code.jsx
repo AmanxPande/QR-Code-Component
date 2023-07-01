40be0404a2211acad69a7531fb56dd6b527ee186
Aman Pande <amanpande912@gmail.com>
2023-07-01 16:28:14 +0530

    setup web app

---
+++ src/components/Code.jsx
@@ -0,0 +1,16 @@
+ 
+ 
+ import React from "react";
+ import Image from "../image/qrImage.png";
+
+ export default function Code() {
+   return (
+     <div className="Code">
+     <img clasName="heroImage" src={Image} alt="hero-img" />
+     <h1>Improve your front-end <br/>skills by buliding projects</h1>
+     <p>Scan the QR code to visit Frontend <br/>Mentor and take your coding skills to <br/>the next level</p>
+      
+     </div>
+   );
+ }
+ 
